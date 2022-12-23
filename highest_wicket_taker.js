8f12ba4e9a72da8f5bb7015565d910f8aad87393
AnirudhaPatil-1 <aninp2020@gmail.com>
2022-12-23 19:16:07 +0530

    task - highest wicket taker completed

--- highest_wicket_taker.js
+++ highest_wicket_taker.js
@@ -15,15 +15,65 @@ function extractHTML(html){
     let $ = cheerio.load(html);
     //$ does the full page search
     let teamsArr = $(".ci-team-score.ds-flex.ds-justify-between.ds-items-center.ds-text-typo-title.ds-mb-2");
+    let winningTeamName;
     for(let i = 0; i < teamsArr.length; i++){
         let hasClass = $(teamsArr[i]).hasClass("ds-opacity-50");
         if(hasClass == false){
             //winning team
             //find does the search inside a container
             let teamNameElem = $(teamsArr[i]).find(".ds-font-bold");
-            console.log(teamNameElem.text());
+            winningTeamName = teamNameElem.text().trim();
+            // console.log(teamNameElem.text());
         }
     }
+    let inningsArr = $(".ds-rounded-lg.ds-mt-2");
+    let htmlStr = "";
+    for(let i = 0; i < inningsArr.length; i++){
+        // let currentHTML = $(inningsArr[i]).html();
+        // htmlStr += currentHTML;
+        //team names
+        let teamNameElem = $(inningsArr[i]).find(".ds-text-title-xs.ds-font-bold.ds-capitalize");
+        let teamName = teamNameElem.text();
+        // console.log(teamName);
+        teamName = teamName.trim();
+        //team table
+        //console.log(teamName);
+
+
+        let highestWicketTaken = 0;
+        let highestWicketTakerName = "";
+        if(teamName == winningTeamName){
+            //winning team 
+            // console.log(teamName); 
+            let winningTeamTables = $(inningsArr[i]).find(".ds-w-full.ds-table.ds-table-md.ds-table-auto");
+            // console.log(winningTeamTables.length);
+            for(let i = 0; i < winningTeamTables.length; i++){
+                let hasClass = $(winningTeamTables[i]).hasClass("ci-scorecard-table");
+                if(hasClass == false){
+                    // bowling table
+                    let wBowlingTable = winningTeamTables[i];
+                    // console.log(wBowlingTable);
+                    let allBowlers = $(wBowlingTable).find("tr");
+                    // console.log(allBowlers.length);
+                    for(let j = 0; j < allBowlers.length; j++){
+                        let allColsOfPlayer = $(allBowlers[j]).find("td");
+                        // console.log(allColsOfPlayer.length);
+                        let playerName = $(allColsOfPlayer[0]).text();
+                        let wickets = $(allColsOfPlayer[4]).text();
+                        // console.log(playerName + " " + wickets);
+                        if(wickets >= highestWicketTaken){
+                            highestWicketTaken = wickets;
+                            highestWicketTakerName = playerName;
+                        }
+                    } 
+                    console.log(`Winning team ${winningTeamName} playerName: ${highestWicketTakerName} wickets: ${highestWicketTaken}`);
+
+                }
+            }
+        }
+    }
+    // console.log(htmlStr);
+
 }
 
 // console.log("after");
--- steps.js
+++ steps.js
@@ -2,4 +2,10 @@
 1. npm init -y
 2. npm i cheerio
 3.npm i request
+4. after getting content in string form in variable HtmlStr copy manual
+the output using console in newly created file named "table.html"
+5. right click in file and select "format document" option
+6. then open with live server
+
+
 */
