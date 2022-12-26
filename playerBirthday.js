afaa6d99811ff0edd610b167e9464ad9e07a5ec6
AnirudhaPatil-1 <aninp2020@gmail.com>
2022-12-26 22:54:30 +0530

    task - find players birthday completed

--- playerBirthday.js
+++ playerBirthday.js
@@ -14,16 +14,67 @@ function cb(err, response, html){
 function extractHTML(html){
     let $ = cheerio.load(html);
     // let teams = [];
-    let teamsArr = $(".ds-w-full.ds-bg-fill-content-prime.ds-overflow-hidden.ds-border.ds-border-line.ds-mb-4");
-    // console.log(teamsArr.length);
-    for(let i = 0; i < teamsArr.length; i++){
-        let hasClass = $(teamsArr[i]).hasClass(".ds-flex.ds-px-4.ds-border-b.ds-border-line.ds-py-3.ds-bg-ui-fill-translucent-hover");
-        if(hasClass == true){
-            // let team = teamsArr[i];
-            // console.log(team.length);
-            
-        }
+    let teamsBatBowlTable = $(".ds-rounded-lg.ds-mt-2 .ds-p-0");
+    let currentHTML = "";
+    currentHTML += $(teamsBatBowlTable).html();
+    // console.log(currentHTML);
+    // console.log(teamsBatBowlTable.length);
+    let htmlStr = "";
+    
+    let batting = $(teamsBatBowlTable).find(".ds-w-full.ds-table.ds-table-md.ds-table-auto.ci-scorecard-table");
+    // console.log(batting.length);
+    let rows = $(batting[0]).find("tbody>tr");
+    // console.log(rows.length);
+    let links = [];
+    for(let i = 0; i < rows.length; i++){
+        let isLink = $(rows[i]).find("a").hasClass("ds-inline-flex ds-items-start ds-leading-none");
+        // console.log(isLink);
+        if(isLink == true){
+            let playerHref = $(rows[i]).find("a").attr("href");
+            // console.log(playerHref);
+            let fullLink = "https://www.espncricinfo.com/" + playerHref;
+            links.push(fullLink); 
+            getBirthdayPage(fullLink);       
+        } 
+        // let playerName = $(rows[i]).find("span").text();
+        // playerName = playerName[0];
+        
+        // console.log("playerName: " + playerName);
+        // let playerHref = $(rows[i]).find("a").attr("href");
+        // console.log(playerName + "link: " + playerHref);
+        // console.log(playerHref);
     }
-    console.log(teamsArr.length);
+    
+
+
+    
 
 }
+
+function  getBirthdayPage(url){
+    request(url, cb);
+    function cb(err, response, html){
+        if(err){
+            console.log(err);
+        }else{
+            extractBirthday(html);
+        }
+    }
+}
+
+function extractBirthday(html){
+    let $ = cheerio.load(html);
+    let playerNameBox = $(".ds-pt-8.ds-px-6.ds-pb-2.ds-text-raw-white");
+    // console.log(playerNameBox.length);
+    let playerName = $(playerNameBox).find(".ds-text-title-l.ds-font-bold").text();
+    // console.log(playerName);
+    let playerBox = $(".ds-grid.ds-grid-cols-2.ds-gap-4.ds-mb-8");
+    let playerDetailsArr = $(playerBox).find(".ds-text-title-s.ds-font-bold.ds-text-ui-typo");
+    let playerBD = $(playerDetailsArr[1]).text();
+
+    // console.log( playerName + " " + playerBD);
+    console.log(`${playerName}  was born on ${playerBD}`);
+    // console.log(playerPerDetails.length);
+
+}
+
